172d0f5b78f911b1630f7932cbcc50da071162a2
Natalia Miranda <natumiranda.28@gmail.com>
2025-12-11 21:12:21 -0300

    se crearon las vistas 1.0

--- tiendaderopa/urls.py
+++ tiendaderopa/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from principal.views import lista_productos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("productos/", lista_productos, name="lista_productos"),
 ]

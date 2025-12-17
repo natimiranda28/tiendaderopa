c2a383b5f9cc6d8478e8712fbfa7867fef442b78
Natalia Miranda <natumiranda.28@gmail.com>
2025-12-17 18:11:48 -0300

    se agrego "ver carrito"

--- principal/views.py
+++ principal/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
-from .models import Producto
+from .models import Producto, Carrito
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, "lista_productos.html", {"productos": productos})
 
+@login_required
+def ver_carrito(request):
+    carrito_items = Carrito.objects.filter(usuario=request.user)
+    total = sum(item.producto.precio * item.cantidad for item in carrito_items)
+    return render(request, "carrito.html", {
+        "carrito_items": carrito_items,
+        "total": total
+    })
